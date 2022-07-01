9de5f8cb4d0fb9fa31d4709e0c4c9614512b992d
Yohann Bacha <bachrc@dessert.coffee>
2022-07-01 22:05:29 +0200

    Ouais ouais

--- src/components/TdsChart.vue
+++ src/components/TdsChart.vue
@@ -1,69 +1,62 @@
 <script lang="ts">
 import { defineComponent } from "vue"
-import { Line } from 'vue-chartjs'
+import { Scatter } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
-import {Extraction} from "../models"
+import { Extraction } from "../models"
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement)
 
 class Releve {
-    private releveTds: number;
-    private poidsCafe: number;
-    private poidsBoisson: number;
+  private releveTds: number;
+  private poidsCafe: number;
+  private poidsBoisson: number;
 
-    constructor(releveTds: number, poidsCafe: number, poidsBoisson: number) {
-        this.releveTds = releveTds;
-        this.poidsCafe = poidsCafe;
-        this.poidsBoisson = poidsBoisson;
-    }
+  constructor(releveTds: number, poidsCafe: number, poidsBoisson: number) {
+    this.releveTds = releveTds;
+    this.poidsCafe = poidsCafe;
+    this.poidsBoisson = poidsBoisson;
+  }
 
-    get yield(): number {
-        return this.releveTds * (this.poidsCafe / this.poidsBoisson)
-    }
+  get yield(): number {
+    return this.releveTds * (this.poidsCafe / this.poidsBoisson)
+  }
 }
 
 export default defineComponent({
-    components: {Line},
-    props: {
-        extractionId: {
-            type: String,
-            required: true
-        }
-    },
-    computed: {
-        extraction() : Extraction {
-            return this.$store.getters.getExtractionById(this.extractionId)
-        }
-    },
-    data() {
-        return {
-            chartOptions: {
-                responsive: true
-            },
-            plugins : [],
-            datasetIdKey: "tds",
-            cssClasses: "",
-            styles: {},
-            chartData: {
-                labels: [ 'January', 'February', 'March' ],
-                datasets: [ { data: [40, 20, 12] } ]
-            },
-        }
+  components: { Scatter },
+  props: {
+    extractionId: {
+      type: String,
+      required: true
     }
+  },
+  computed: {
+    extraction(): Extraction {
+      return this.$store.getters.getExtractionById(this.extractionId)
+    }
+  },
+  data() {
+    return {
+      chartOptions: {
+        responsive: true
+      },
+      plugins: [],
+      datasetIdKey: "tds",
+      cssClasses: "",
+      styles: {},
+      chartData: {
+        datasets: [{
+          data: [{ x: 1, y: 3 }, { x: 3, y: 4 }, { x: 4, y: 6 }, { x: 6, y: 9 }],
+          showLine: true
+        }]
+      },
+    }
+  }
 })
 
 </script>
 
 <template>
-    <Line
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-        chart-id="tds-chart"
-        :dataset-id-key="datasetIdKey"
-        :plugins="plugins"
-        :css-classes="cssClasses"
-        :styles="styles"
-        :width=400
-        :height=400
-    />
+  <Scatter :chart-options="chartOptions" :chart-data="chartData" chart-id="tds-chart" :dataset-id-key="datasetIdKey"
+    :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width=400 :height=400 />
 </template>
--- src/components/extractions/EditExtraction.vue
+++ src/components/extractions/EditExtraction.vue
@@ -6,56 +6,59 @@ import TdsChart from "../TdsChart.vue"
 import { MutationType } from "../../mutations"
 
 export default defineComponent({
-    computed: {
-      extractionId(): string {
-        return this.$route.params.id as string;
-      },
-      releves(): number[] {
-        return this.$store.getters.getRelevesFromExtraction(this.extractionId)
-      }
+  computed: {
+    extractionId(): string {
+      return this.$route.params.id as string;
     },
-    methods: {
-      updatePoidsCafe(poids: number) {
-        this.$store.commit(MutationType.UPDATE_POIDS_CAFE, {extractionId: this.extractionId, poids})
-      },
-      updatePoidsBoisson(poids: number) {
-        this.$store.commit(MutationType.UPDATE_POIDS_BOISSON, {extractionId: this.extractionId, poids})
-      },
-      addTds() {
-        this.$store.commit(MutationType.ADD_TDS_TO_EXTRACTION, this.extractionId)
-      },
-      updateTds(value: number, index: number) {
-        this.$store.commit(MutationType.UPDATE_TDS, {extractionId: this.extractionId, index, value})
-      },
-      removeTds(index: number) {
-        this.$store.commit(MutationType.REMOVE_TDS, {extractionId: this.extractionId, index})
-      }
+    releves(): number[] {
+      return this.$store.getters.getRelevesFromExtraction(this.extractionId)
+    }
+  },
+  methods: {
+    updatePoidsCafe(poids: number) {
+      this.$store.commit(MutationType.UPDATE_POIDS_CAFE, { extractionId: this.extractionId, poids })
     },
-    components: {
+    updatePoidsBoisson(poids: number) {
+      this.$store.commit(MutationType.UPDATE_POIDS_BOISSON, { extractionId: this.extractionId, poids })
+    },
+    addTds() {
+      this.$store.commit(MutationType.ADD_TDS_TO_EXTRACTION, this.extractionId)
+    },
+    updateTds(value: number, index: number) {
+      this.$store.commit(MutationType.UPDATE_TDS, { extractionId: this.extractionId, index, value })
+    },
+    removeTds(index: number) {
+      this.$store.commit(MutationType.REMOVE_TDS, { extractionId: this.extractionId, index })
+    }
+  },
+  components: {
     InputField,
     Button,
     TdsChart
-}
+  }
 })
 </script>
 
 <template>
-<div class="flex md:flex-row flex-col">
-  <section class="w-1/2 flex flex-col">
-    <div class="flex flex-row">
-      <InputField type="number" label="Poids de café" placeholder="Poids de café" unit="g" value="" class="w-1/2" @input="updatePoidsCafe" />
-      <InputField type="number" label="Poids de la boisson" placeholder="Poids de boisson" value="" unit="g" class="w-1/2" @input="updatePoidsBoisson" />
-    </div>
-    <div v-for="(releve, index) in releves" class="flex flex-row">
-      <InputField type="number" :label="'Relevé ' + (index + 1)" placeholder="Relevé de TDS" unit="%" :value="releve.toString()" @input="(value: number) => updateTds(value, index)" />
-      <Button text="Supprimer.." @click="() => removeTds(index)" />
-    </div>
-    <div>
-      <Button text="Ajouter un relevé" @click="addTds" />
-    </div>
-  </section>
-  <section class="w-1/2">
-    <TdsChart />
-  </section>  
-</div>
+  <div class="flex md:flex-row flex-col">
+    <section class="w-1/2 flex flex-col">
+      <div class="flex flex-row">
+        <InputField type="number" label="Poids de café" placeholder="Poids de café" unit="g" value="" class="w-1/2"
+          @input="updatePoidsCafe" />
+        <InputField type="number" label="Poids de la boisson" placeholder="Poids de boisson" value="" unit="g"
+          class="w-1/2" @input="updatePoidsBoisson" />
+      </div>
+      <div v-for="(releve, index) in releves" class="flex flex-row">
+        <InputField type="number" :label="'Relevé ' + (index + 1)" placeholder="Relevé de TDS" unit="%"
+          :value="releve.toString()" @input="(value: number) => updateTds(value, index)" />
+        <Button text="Supprimer.." @click="() => removeTds(index)" />
+      </div>
+      <div>
+        <Button text="Ajouter un relevé" @click="addTds" />
+      </div>
+    </section>
+    <section class="w-1/2">
+      <TdsChart extraction-id="extractionId" />
+    </section>
+  </div>
 </template>
